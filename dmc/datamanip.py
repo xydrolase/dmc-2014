e52fd5944f1ea55e8348206880bc2f3a9bac308e
Xin Yin <killkeeper@gmail.com>
2014-04-18 23:52:30 -0500

    Now extracts features related to historical purchasing behaviors.

--- dmc/datamanip.py
+++ dmc/datamanip.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from collections import Counter
 from math import log
 
 import numpy as np
@@ -9,6 +10,27 @@ import pandas as pd
 
 def feature_name(feats, sep='_'):
     return feats if type(feats) is str else sep.join(feats)
+
+def batch_counter(df, order_cnts, rt_cnts, columns):
+    groups = df.groupby(columns)
+    feat_return = groups['return'].agg(sum)
+    feat_order = groups.size()
+
+    ordered_times = pd.DataFrame([(rt_cnts[x], order_cnts[x]) 
+                     for x in df[columns].itertuples()], index=df.index)
+
+    for feat, crt, cord in zip(groups.groups.keys(), feat_return, feat_order):
+        order_cnts[feat] += cord
+        rt_cnts[feat] += crt
+
+    return ordered_times
+
+def sequential_counter(row, counter):
+    ntuple = tuple(row)
+    n = counter[ntuple]
+    counter[ntuple] += 1
+
+    return (n > 0, n)
 
 def trans_unique_count(x):
     return pd.Series(np.repeat(len(set(x)), len(x)), index=x.index)
@@ -26,7 +48,7 @@ def trans_llr(x, c1, c2):
 
 def batch_summarize(bat_df, u_feats, wi_feats):
     # a) per batch features: U.iid, U.size ...
-    u_names = map(lambda n: 'U_{0}'.format(n),
+    u_names = map(lambda n: 'bat.uniq.{0}'.format(n),
         [x if type(x) is str else '_'.join(x) for x in u_feats])
 
     # aggregation of all per batch features
@@ -39,12 +61,16 @@ def batch_summarize(bat_df, u_feats, wi_feats):
         _group = bat_df.groupby(wi_feat, sort=False)
 
         for _feat in agg_feats:
-            wi_name = "WI{0}_{1}".format(wi_feat, feature_name(_feat))
+            wi_name = "bwi_{0}.uniq.{1}".format(wi_feat, feature_name(_feat))
             wi_data = _group[_feat].apply(trans_unique_count)
 
             data.append((wi_name, wi_data))
 
     # IMPORATANT: mirroring index!
     df = pd.DataFrame(dict(data), index=bat_df.index)
+
+    # c) other simple features 
+    df['bat.n'] = len(bat_df)
+    df['bat.sum.return'] = sum(bat_df['return'])
 
     return df
--- feature_extract.py
+++ feature_extract.py
@@ -12,7 +12,7 @@ from collections import defaultdict, Counter
 from itertools import combinations, izip 
 from argparse import ArgumentParser 
 
-from dmc.datamanip import trans_group_count, trans_llr, batch_summarize
+from dmc.datamanip import * 
 
 import os
 import logging
@@ -21,17 +21,21 @@ import pandas as pd
 import numpy as np
 
 ### Some options, later will be moved into ArgumentParser
-GLOBAL_FEATURES = ['iid']
-#GLOBAL_FEATURES = ['iid', 'size', 'color', ('iid', 'size'), 
-#                   ('iid', 'color')]
+#GLOBAL_FEATURES = ['iid']
+GLOBAL_FEATURES = ['mid', 'iid', 'size', 'color', ('iid', 'size'), 
+                   ('iid', 'color')]
 
 BATCH_FEATURES = ['iid', 'size', 'color', 'mid']
 
 WITHIN_FEATURES = {
     'iid': ['size', 'color'],
-    #'size': ['iid', 'mid'],
-    #'color': ['iid', 'mid']
+    'size': ['iid', 'mid'],
+    'color': ['iid', 'mid']
 }
+
+#HISTORICAL_FEATURES = ['iid']
+HISTORICAL_FEATURES = ['iid', 'size', 'color', 
+                       ('iid', 'size'), ('iid', 'color')]
 
 def parse_options():
     parser = ArgumentParser(
@@ -62,15 +66,15 @@ def main():
     cid_edge[cid_edge != 0] = 1
     train['batch'] = np.cumsum(cid_edge)
 
-    train_test = train.iloc[:100, :]
+    # for test only
+    #train = train.iloc[:1000, :]
 
     # 3) whole dataset aggregation
     for feat in GLOBAL_FEATURES:
-        break
         feat_join = feat if type(feat) is str else "_".join(feat)
 
-        name_S = "S_{0}".format(feat_join)
-        name_LLR = "LLR_{0}".format(feat_join)
+        name_S = "all.count.{0}".format(feature_name(feat))
+        name_LLR = "all.llr.{0}".format(feature_name(feat))
 
         _group = train.groupby(feat, sort=False)
 
@@ -82,12 +86,44 @@ def main():
 
     # 4) per batch summary statistics and ...
     # 5) features within substructures of batch
-
-    batches = train.groupby('batch')
+    batches = train.groupby('batch', sort=False)
     bat_feats = batches.apply(
         batch_summarize, u_feats=BATCH_FEATURES, wi_feats=WITHIN_FEATURES)
 
-    print(bat_feats.head(n=50))
+    # 6) historical features
+    #    number of times an item had been purchased by customer
+    #    number of batches an item had been purchased by customer
+    #    number of times returned
+    #    had been purchased?
+    #    had been returned?
+    hist_feat_list = []
+    for feat in HISTORICAL_FEATURES:
+        hist_row_cnts = defaultdict(int)
+        hist_batch_cnts = defaultdict(int)
+        hist_batch_returns = defaultdict(int)
+
+        cols = ['cid']
+        if type(feat) in (tuple, list):
+            cols.extend(feat)
+        elif type(feat) is str:
+            cols.append(feat)
+
+        #hist_feats = train[cols].apply(
+        #    sequential_counter, counter=hist_row_cnts, axis=1)
+
+        batch_hist_feats = batches.apply(
+            batch_counter, order_cnts=hist_batch_cnts, 
+            rt_cnts=hist_batch_returns, columns=cols)
+
+        batch_hist_feats.columns = [
+            'bhist.purcnt.{0}'.format(feature_name(feat)),
+            'bhist.retcnt.{0}'.format(feature_name(feat))]
+
+        hist_feat_list.append(batch_hist_feats)
+
+    # concatenate features and dump
+    feat_mat = pd.concat([train, bat_feats] + hist_feat_list, axis=1)
+    feat_mat.to_csv("out.csv", index=False)
 
 if __name__ == "__main__":
     main()
