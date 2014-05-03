d4840a81647b8cb4d28b98115814d1beec6abf45
Xin Yin <killkeeper@gmail.com>
2014-05-03 16:10:58 -0500

    First pass of the feature matrix generation R code.

--- feat_gen.R
+++ feat_gen.R
@@ -35,19 +35,51 @@ counts.and.llrs <- function(df, feats, c1=1.0, c2=1.0) {
     grp <- do.call(group_by, c(list(df), list(feats)))
 
     # overall counts and returns, sans validation set
-    N <- grp %.% mutate(counts=sum(!is.na(return))) 
-    R <- grp %.% mutate(returns=sum(return, na.rm=T))
+    N <- (grp %.% mutate(counts=sum(!is.na(return))))$counts
+    R <- (grp %.% mutate(returns=sum(return, na.rm=T)))$returns
 
     # per batch counts (which will be used to compute correction factor)
     bat.grp <- do.call(group_by, c(list(df), list(c('batch', feats))))
-    k <- bat.grp %.% mutate(counts=sum(!is.na(return)))
+    k <- (bat.grp %.% mutate(counts=sum(!is.na(return))))$counts
 
-    llr <- log((R$returns + c1) / (N$counts - R$returns + c2))
-    adj.llr <- (counts - k) / counts * llr
+    llr <- log((R + c1) / (N - R + c2))
+    adj.llr <- (N - k) / N * llr
 
-    return (cbind(N$counts, adj.llr))
+    return (as.data.frame(cbind(N, adj.llr)))
 }
 
+## historical features
+all.cols <- c("cid", "iid", "mid", "ztype", "zsize", "size", "color", 
+               "state", "month", "season", "dow")
+feats.2way <- combn(all.cols, 2)
+
+all.feats <- NULL
+
+for (cidx in all.cols) {
+    fnames <- paste(c("all.cnt.", "all.llr."), cidx, sep="") 
+    if (is.null(all.feats)) {
+        all.feats <- counts.and.llrs(raw.tr, cidx)
+        names(all.feats) <- fnames
+    } else {
+        .feats = counts.and.llrs(raw.tr, cidx)
+        names(.feats) <- fnames
+        all.feats <- cbind(all.feats, .feats)
+    }
+}
+
+for (i in 1:ncol(feats.2way)) {
+    cols <- feats.2way[, i]
+    cat(" :: ", cols, fill=T)
+
+    fnames <- paste(c("all.cnt.", "all.llr."), 
+                    paste(cols, collapse="_"), sep="") 
+
+    .feats = counts.and.llrs(raw.tr, cols)
+    names(.feats) <- fnames
+    all.feats <- cbind(all.feats, .feats)
+}
+
+names(all.feats)
 
 ## batch features
 bfeats <- batches %.% mutate(bat.n=length(oid), 
@@ -58,8 +90,6 @@ bfeats <- batches %.% mutate(bat.n=length(oid),
                    bat.uniq.ztype=length(unique(ztype)),
                    bat.uniq.zsize=length(unique(zsize)),
                    bat.prank=rank(price))
-
-## within batch features
 
 ## customer per batch features
 cbatches <- group_by(raw.tr, cid, batch)
@@ -81,17 +111,20 @@ cb.ret.rates$skrate <- cb.ret.srates$skrate
 
 # average return/keep rate, weighted and unweighted,
 cb.avg.feats <- group_by(cb.ret.rates, cid) %.%
-                murate(cid.wavg.rrate=mean(rrate, na.rm=T),
+                mutate(cid.wavg.rrate=mean(rrate, na.rm=T),
                        cid.wavg.krate=mean(krate, na.rm=T),
                        # simple averages
                        cid.avg.rrate=mean(srrate, na.rm=T),
                        cid.avg.krate=mean(skrate, na.rm=T),
                        cid.sum.rrate=sum(srrate, na.rm=T),
                        cid.sum.krate=sum(skrate, na.rm=T)) %.%
-                select(cid, starts_with('cid.'))
+                select(cid, batch, starts_with('cid.'))
 
 # log-likelihood ratio of return over kept
 cb.avg.feats$cid.llr.rk <- log((cb.avg.feats$cid.avg.rrate+1) /
                                (cb.avg.feats$cid.avg.krate+1))
 
+feat.mat <- cbind(raw.tr, all.feats, bfeats[, -1], cb.avg.feats[, -2])
 
+# output
+write.csv(feat.mat, file="featmatrix_v4_part1.csv", row.names=F)
